3a4fe29b120ca78b99c1a1ce7f5ceca8eb49b4b6
AlexysCoate <42352943+AlexysCoate@users.noreply.github.com>
2019-09-02 10:22:08 -0500

    Add files via upload

---
+++ TestGame.java
@@ -0,0 +1,12 @@
+
+package T03_Bags;
+
+
+public class TestGame {
+    
+    public static void main(String[] args) {
+        //Crea una clase BallGame2 e inicializa el juego
+        BallGame2 Game = new BallGame2(5);
+        Game.startGame();
+    }
+}
